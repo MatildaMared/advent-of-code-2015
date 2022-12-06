e638fa20430011db2887f51db3b1e9fea2be35d6
Matilda Mared <matildamared@live.se>
2022-12-06 15:22:52 +0100

    initialize project

---
+++ src/01/index.ts
@@ -0,0 +1,10 @@
+import * as fs from "fs";
+import * as path from "path";
+
+// 🎄 🎅 Advent of Code 2015 Day 1 🎅 🎄
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+
+// 🍬 🍭 Part 1 🍭 🍬
+
+// 🍬 🍭 Part 2 🍭 🍬
